cbba448571d5c626f55b474a4640795b15a89712
Lenovo <dieter.vanderelst@fastmail.net>
2021-06-26 13:23:40 -0400

    first steps

--- test.py
+++ test.py
@@ -1,5 +1,43 @@
-import textgraph
-data = [120, 5, 2, 7]
+import usocket
+import time
+import network
+import gc
+import Misc
+import microdot
+import Measure
+import Settings
+import Plot
 
-a = textgraph.vertical(data, height=20, character='*')
-pri
+
+def create_access_point():
+    access_point = network.WLAN(1)
+    access_point.config(essid='PYBD')          # set AP SSID
+    access_point.config(password='pybd0123')   # set AP password
+    access_point.config(channel=6)             # set AP channel
+    access_point.active(1)                     # enable the AP
+    while access_point.isconnected() == False: pass
+    print('Connection successful')
+    print(access_point.ifconfig())
+    return access_point
+
+
+
+
+app = microdot.Microdot()
+
+
+
+
+@app.route('/')
+def hello(request):
+    return microdot.Response(body=htmldoc, headers={'Content-Type': 'text/html'})
+
+
+@app.route('/shutdown')
+def shutdown(request):
+    request.app.shutdown()
+    return 'The server is shutting down...'
+
+
+create_access_point()
+app.run(debug=True, host='127.0.0.0', port=80)
