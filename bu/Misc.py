fb17ef1b78c929d0214d40aa33b585e7152061d3
Lenovo <dieter.vanderelst@fastmail.net>
2022-01-30 10:03:25 -0500

    cleaning

--- bu/Misc.py
+++
@@ -1,9 +0,0 @@
-def lst2str(lst):
-    if isinstance(lst, str): return lst
-    text = ''
-    for x in lst: text += str(x) + ' '
-    text = text.rstrip(' ')
-    return text
-
-def default_function(*args):
-    return args
--- bu/Settings.py
+++
@@ -1,4 +0,0 @@
-break_character = '*'
-wifi_ssid = 'ZyXEL39940'
-wifi_password = '2BF0EBC3CC'
-port_number = 1234
